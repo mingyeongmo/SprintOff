58a483a91c09bdf8ae92d696c4b7660384aaf037
minmo <mingyeongmo9854@gmail.com>
2026-05-03 00:47:45 +0900

    폴더구조 잡기

---
+++ src/app/(employee)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EmployeeDashboardPage = () => {
+    return (
+        <div>
+            EmployeeDashboardPage
+        </div>
+    );
+};
+
+export default EmployeeDashboardPage;
